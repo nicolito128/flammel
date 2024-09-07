aaaad3c3366236a738c135e0313df5a7c3b095e7
Nicolas <serna.nicolas.abel@gmail.com>
2024-09-07 01:46:35 -0300

    up: seyfert config

--- seyfert.config.js
+++ seyfert.config.js
@@ -3,7 +3,7 @@ const { config } = require('seyfert')
 
 const getBotToken = () => {
   if (process.env.NODE_ENV === 'production') {
-    return fs.readFileSync('/run/secrets/BOT_TOKEN', 'utf8')
+    return fs.readFileSync('/run/secrets/BOT_TOKEN', 'utf8').trim()
   } else {
     return process.env.BOT_TOKEN ?? ''
   }
@@ -11,7 +11,7 @@ const getBotToken = () => {
 
 const getApplicationId = () => {
   if (process.env.NODE_ENV === 'production') {
-    return fs.readFileSync('/run/secrets/APPLICATION_ID', 'utf8')
+    return fs.readFileSync('/run/secrets/APPLICATION_ID', 'utf8').trim()
   } else {
     return process.env.APPLICATION_ID ?? ''
   }
